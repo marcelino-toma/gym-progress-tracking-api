17d14a4cfc0f0d2c2b2c3060fdc85f546f8e70b7
marcelino-toma <marcelinotoma.exe@gmail.com>
2025-12-14 15:41:09 +0200

    WIP for initial setup

---
+++ api/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/marcelino-toma/gym-progress-tracking-api/foundation/conf"
+)
+
+func main() {
+	fmt.Println("hello")
+
+	cfg := conf.New()
+	err:= cfg.Parse()
+	if err != nil {
+		log.Print(err)
+	}
+}
